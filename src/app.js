84714dd293c385194726a56a7f8166deaa2b6b08
Nandhini_T <129613480+NandhuTee@users.noreply.github.com>
2024-06-10 22:28:53 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -5,15 +5,17 @@ const port = 3000;
 app.get('', (req, res) => {
   // TODO: Implement handling of the root endpoint '/'
   // Replace this comment with your code to send "Hello World!"
+  res.send('Hello World');
 });
 
 app.get('', (req, res) => {
   // TODO: Implement handling of the '/about' endpoint
   // Replace this comment with your code to send "Welcome to about page"
+  res.send('welcome to about page');
 });
 
 app.listen(port, () => {
-  console.log(`app listening at ${port}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
 
 // Export the Express app for testing purposes
