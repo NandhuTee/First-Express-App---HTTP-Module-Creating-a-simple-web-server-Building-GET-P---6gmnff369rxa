844a87e3a2184bbfbdb159d5c29326ef21daaeef
Nandhini_T <129613480+NandhuTee@users.noreply.github.com>
2024-06-10 23:06:11 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,21 +1,16 @@
+// src/app.js
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 3000; // Using `port` as a consistent variable name
 
+// Root endpoint
 app.get('/', (req, res) => {
-  // TODO: Implement handling of the root endpoint '/'
-  // Replace this comment with your code to send "Hello World!"
-  res.send('Hello World');
+  res.send('Hello World!');
 });
 
+// About endpoint
 app.get('/about', (req, res) => {
-  // TODO: Implement handling of the '/about' endpoint
-  // Replace this comment with your code to send "Welcome to about page"
-  res.send('welcome to about page');
-});
-
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  res.send('Welcome to about page');
 });
 
 // Export the Express app for testing purposes
